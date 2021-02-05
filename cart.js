cd6e8e72aa25a65c718a177c18263cddbc34063f
inshalbaig <73382175+inshalbaig@users.noreply.github.com>
2021-02-06 04:45:45 +0500

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,12 @@
+(function(){
+
+    let function = document.querySelector('function');
+    let container = document.querySelector('.container');
+    let body = document.querySelector('body');
+    
+    body.addEventListener(function(Math.floor(.container))){
+        document.querySelector('.container').addEventListener
+        body.button(e).function(floor.Math.random(container))
+        document.querySelector('body').function(body).function
+    }
+})
---
+++ demo.js
@@ -0,0 +1,23 @@
+(function(){
+    
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+    
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            screen.value.function(e)
+            screen.value = "Please Enter a Value"{
+                button.addEventListener(e).Math.random.length
+                value.function(button){
+                    onclick.btn
+                    document.querySelector('click', btn)
+                    screen.value(e)
+                    
+                }
+            }
+        })
+    })
+    
+})();
